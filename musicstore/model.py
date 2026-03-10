70603ba3c16f079375fafc7eb6e632d4c16209f5
Miguel Obando <mynameismiguel777@gmail.com>
2026-03-09 21:02:38 -0500

    tercera parte

--- musicstore/model.py
+++ musicstore/model.py
@@ -1,26 +1,52 @@
 from datetime import datetime
 from datetime import datetime
 
+
 class Transaction:
     SELL = 1
     SUPPLY = 2
 
-    def __init__(self, type, copies):
+    def __init__(self, type: int, copies: int):
         self.type = type
         self.copies = copies
         self.date = datetime.now()
 
-    class Disc:
 
-        def __init__(self, sid: str, title: str, artist: str, sale_price: float, purchase_price: float, quantity: int):
-            self.sid = sid
-            self.title = title
-            self.artist = artist
-            self.sale_price = sale_price
-            self.purchase_price = purchase_price
-            self.quantity = quantity
-            self.transactions = []
-            self.song_list = []
+class Disc:
+
+    def __init__(self, sid: str, title: str, artist: str,
+                 sale_price: float, purchase_price: float, quantity: int):
+
+        self.sid = sid
+        self.title = title
+        self.artist = artist
+        self.sale_price = sale_price
+        self.purchase_price = purchase_price
+        self.quantity = quantity
+
+        self.transactions = []
+        self.song_list = []
 
     def add_song(self, song: str):
         self.song_list.append(song)
+
+    def sell(self, copies: int):
+        if copies > self.quantity:
+            return False
+
+        self.quantity -= copies
+        t = Transaction(Transaction.SELL, copies)
+        self.transactions.append(t)
+        return True
+
+    def supply(self, copies: int):
+        self.quantity += copies
+        t = Transaction(Transaction.SUPPLY, copies)
+        self.transactions.append(t)
+
+    def copies_sold(self) -> int:
+        total = 0
+        for t in self.transactions:
+            if t.type == Transaction.SELL:
+                total += t.copies
+        return total
